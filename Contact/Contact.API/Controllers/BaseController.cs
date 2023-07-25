3d405b75abce5576b399537f355d7f2021bc1f17
Tarlan Usubov <Tarlan Usubov@DESKTOP-RRKMAGR>
2023-07-25 07:29:03 -0700

    change

--- Contact/Contact.API/Controllers/BaseController.cs
+++ Contact/Contact.API/Controllers/BaseController.cs
@@ -29,7 +29,7 @@ namespace Contact.API.Controllers
             }
             var cookie = new CookieOptions()
             {
-                Domain = "localhost",
+                Domain = "*.hra.az",
                 Path = "/",
                 HttpOnly = true,
                 Secure = true,
